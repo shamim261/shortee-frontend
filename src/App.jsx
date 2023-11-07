57cdc5c2440e8f89cf7145d9ef1b592804b8ca84
Shamim <mdshamimreza5552@gmail.com>
2023-11-08 01:28:41 +0600

    Add private and public route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import PrivateOutlet from './components/PrivateOutlet';
+import PublicOutlet from './components/PublicOutlet';
 import Auth from './pages/auth';
 import Dashboard from './pages/dashboard';
 import Home from './pages/home';
@@ -10,8 +12,12 @@ function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/dashboard" element={<PrivateOutlet />}>
+                        <Route index element={<Dashboard />} />
+                    </Route>
+                    <Route path="/auth" element={<PublicOutlet />}>
+                        <Route index element={<Auth />} />
+                    </Route>
                 </Routes>
             </Layout>
         </BrowserRouter>
--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,15 +1,20 @@
+import Form from './Form';
+
 export default function MainInput() {
     return (
         <>
             <div className="">
-                <input
-                    type="text"
-                    className="p-4 w-80 text-lg md:w-[30rem] md:p-6 m-4 rounded-l-xl md:placeholder:text-lg focus:outline-none"
-                    placeholder="Enter your URL"
-                />
-                <button className="p-4 md:py-6 bg-primary hover:bg-hover transition-colors uppercase font-bold text-slate-100 -ml-4 rounded-r-xl md:text-lg">
-                    Shorten URL
-                </button>
+                <Form>
+                    <input
+                        required
+                        type="text"
+                        className="p-4 w-80 text-lg md:w-[30rem] md:p-6 m-4 rounded-l-xl md:placeholder:text-lg focus:outline-none"
+                        placeholder="Enter your URL"
+                    />
+                    <button className="p-4 md:py-6 bg-primary hover:bg-hover transition-colors uppercase font-bold text-slate-100 -ml-4 rounded-r-xl md:text-lg">
+                        Shorten URL
+                    </button>
+                </Form>
             </div>
         </>
     );
