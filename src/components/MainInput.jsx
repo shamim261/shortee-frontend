e36adc734114132c546885e3ce9d8d256d923ce8
Shamim <mdshamimreza5552@gmail.com>
2023-11-08 02:12:13 +0600

    Fetch URLs from API

--- src/components/MainInput.jsx
+++ src/components/MainInput.jsx
@@ -1,11 +1,38 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Form from './Form';
 
 export default function MainInput() {
+    const [url, setUrl] = useState('');
+    const [error, setError] = useState();
+    const userInfo = localStorage.getItem('userInfo');
+    const navigate = useNavigate();
+
+    async function submitHandler(e) {
+        e.preventDefault();
+        if (!userInfo) {
+            navigate('/auth');
+        }
+        try {
+            const { data } = await axios.post('api/urls', {
+                url: url,
+            });
+            if (data.success) {
+                alert(data.success);
+                navigate('/dashboard');
+            }
+        } catch (err) {
+            setError(err);
+        }
+    }
     return (
         <>
             <div className="">
-                <Form>
+                <Form onSubmit={submitHandler}>
                     <input
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
                         required
                         type="text"
                         className="p-4 w-80 text-lg md:w-[30rem] md:p-6 m-4 rounded-l-xl md:placeholder:text-lg focus:outline-none"
--- src/components/UrlList.jsx
+++ src/components/UrlList.jsx
@@ -1,6 +1,17 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { copyClip, genSlice, truncate } from '../utilities/util';
+import { copyClip, truncate } from '../utilities/util';
 export default function UrlList() {
+    const [urls, setUrls] = useState([]);
+    useEffect(() => {
+        async function getData() {
+            const { data } = await axios.get('api/urls');
+            setUrls(data);
+        }
+        getData();
+    }, []);
+
     return (
         <div className="relative overflow-x-auto">
             <table className="w-full text-md text-left text-gray-500 dark:text-gray-400">
@@ -21,36 +32,41 @@ export default function UrlList() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th
-                            scope="row"
-                            className="px-6 font-normal py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                    {urls.map((url) => (
+                        <tr
+                            key={url._id}
+                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                         >
-                            <Link to="#">
-                                {truncate('https://downforeveryoneorjustme.com/', 20)}
-                            </Link>
-                        </th>
-                        <td className="px-6 py-3 break-words flex">
-                            <Link to="#">
-                                <div className="w-32 md:w-full">
-                                    {genSlice('https://shortee.devshamim.com/n5s8sc8')}
-                                </div>
-                            </Link>
-                            <button
-                                onClick={() => copyClip('test !')}
-                                type="button"
-                                className="relative bottom-1 ml-1 md:border-2 text-hover py-0 rounded-md md:bg-primary md:text-slate-100 md:p-1 md:mb-2"
+                            <th
+                                scope="row"
+                                className="px-6 font-normal py-4 text-gray-900 whitespace-nowrap dark:text-white"
                             >
-                                copy
-                            </button>
-                        </td>
-                        <td className="px-6 py-4 text-center md:text-left">1</td>
-                        <td className="px-6 py-4  ">
-                            <Link to="#" className="ml-1 text-red-500">
-                                Delete
-                            </Link>
-                        </td>
-                    </tr>
+                                <Link to={url.mainURL}>{truncate(url.mainURL, 20)}</Link>
+                            </th>
+                            <td className="px-6 py-3 break-words flex">
+                                <Link to={import.meta.env.VITE_APP_URL + '/' + url.shortURL}>
+                                    <div className="w-32 md:w-full">
+                                        {import.meta.env.VITE_APP_URL + '/' + url.shortURL}
+                                    </div>
+                                </Link>
+                                <button
+                                    onClick={() =>
+                                        copyClip(import.meta.env.VITE_APP_URL + '/' + url.shortURL)
+                                    }
+                                    type="button"
+                                    className="relative bottom-1 ml-1 md:border-2 text-hover py-0 rounded-md md:bg-primary md:text-slate-100 md:p-1 md:mb-2"
+                                >
+                                    copy
+                                </button>
+                            </td>
+                            <td className="px-6 py-4 text-center md:text-left">
+                                {url.visitedHistory.length}
+                            </td>
+                            <td className="px-6 py-4  ">
+                                <Link className="ml-1 text-red-500">Delete</Link>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
